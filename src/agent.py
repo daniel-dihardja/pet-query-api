df7a75e7eb2d668b5628a043f115cdbc5da027bf
Daniel Dihardja <daniel@dihardja.de>
2024-12-23 09:44:14 +0100

    add translation step

--- src/agent.py
+++ src/agent.py
@@ -2,14 +2,15 @@ from typing_extensions import TypedDict
 from langgraph.graph import END, START, StateGraph
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from typing import Annotated
+from typing import Annotated, Literal
 from langgraph.graph.message import add_messages
-from prompt_templates import DETECT_LANGUAGE_PROMPT
+from prompt_templates import DETECT_LANGUAGE_PROMPT, TRANSLATE_USER_QUERY_PROMPT
 
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
     lang: str
+    translated_message: str
 
 
 def create_llm():
@@ -25,13 +26,33 @@ def language(state: State):
     llm = create_llm()
     prompt_template = PromptTemplate.from_template(DETECT_LANGUAGE_PROMPT)
     chain = prompt_template | llm
-    language = chain.invoke({"message": state["messages"][-1]})
-    return {"lang": language}
+    res = chain.invoke({"message": state["messages"][-1]})
+    return {"lang": res.content}
+
+
+def should_translate(state: State) -> Literal["translate", END]:
+    lang = state["lang"]
+    if lang == "de":
+        return END
+    return "translate"
+
+
+def translate(state: State):
+    llm = create_llm()
+    prompt_template = PromptTemplate.from_template(TRANSLATE_USER_QUERY_PROMPT)
+    chain = prompt_template | llm
+    res = chain.invoke({"message": state["messages"][-1], "lang": state["lang"]})
+    return {"translated_message": res.content}
 
 
 graph_workflow = StateGraph(State)
 graph_workflow.add_node("chatbot", chatbot)
 graph_workflow.add_node("language", language)
+graph_workflow.add_node("translate", translate)
+
 graph_workflow.add_edge(START, "language")
+
+graph_workflow.add_conditional_edges("language", should_translate)
 graph_workflow.add_edge("language", END)
+graph_workflow.add_edge("translate", END)
 agent = graph_workflow.compile()
--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -7,3 +7,14 @@ Your task is to detect the language of the following user message and return onl
 
 Output strictly the two-letter language code with no additional text, quotes, or formatting.
 """
+
+
+TRANSLATE_USER_QUERY_PROMPT = """
+Your task is to accurately translate the following message from {lang} to German (de).
+
+<message>
+{message}
+</message>
+
+Provide only the translated text without any additional comments or formatting.
+"""
